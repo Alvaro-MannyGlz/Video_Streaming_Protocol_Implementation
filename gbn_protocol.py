7ed16437463a738ec97bed043ab02866a247dc0a
Jesus Lopez <jesusmax313@gmail.com>
2025-10-17 16:00:43 -0500

    Created outline code for gbn protocol

--- gbn_protocol.py
+++ gbn_protocol.py
@@ -4,3 +4,35 @@
 # Cumulative ACKs
 # Checksums
 # Retransmission Timer
+
+#basic structure for code
+class GBN:
+    def __init__(self, window_size, timeout):
+        self.window_size = window_size
+        self.base = 0
+        self.next_seq_num = 0
+        self.buffer = ()
+        self.timer = None
+
+    def compute_checksun(data: bytes) -> init:
+        checksun = 0
+        for i in range(0, len(data), 2):
+            section = data[i] << 8
+            if i + 1 < len(data):
+                i += 1
+                section += data[i]
+            checksum += section
+            checksum = (checksum & 0xFFFF) + (checksum >> 16)  
+        return ~checksum & 0xFFFF
+
+    def create_packet(self, seq_num, data):
+        pass
+
+    def send_paclet(self, packet):
+        pass
+
+    def receive_ack(self, ack_num):
+        pass
+
+    def timeout(self):
+        pass
