95c0d0884a64aea9cc20197cb37abbc42c2ebe05
Gabriel Garcia <gabevc4@gmail.com>
2025-12-02 22:34:42 -0600

    Update video_server.py

--- streaming_app/server/video_server.py
+++ streaming_app/server/video_server.py
@@ -4,10 +4,74 @@
 
 import sys
 import os
+import rtp_streamer
+import socket 
+import threading
 
 # Add the parent directory to the path so we can see 'shared'
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from shared.gbn_protocol import GBNSender, GBNUtilities
-import rtp_streamer
 
+client_sessions = {}
+
+class VideoServer:
+    def __init__(self, host='0.0.0.0', port=9000):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.bind((self.host, self.port))
+        
+        print(f"[SERVER] Server running on {self.host}:{self.port}")
+    
+    def start(self):
+        print("[SERVER] Waiting for client command...")
+        
+        while True:
+            # Receive command from client
+            data, client_addr = self.server_socket.recvfrom(2048)
+            message = data.decode()
+            print(f"[SERVER] Received from {client_addr}: {message}")
+            
+            # Client command handing
+            threading.Thread(
+                target=self.handle_client,
+                args=(message, client_addr),
+                daemon=True
+            ).start()
+    
+    # Command Parser 
+    def handle_client(self, message, client_addr):
+        parts = message.split(maxsplit=1)
+        command = parts[0]
+        
+        if len(parts) > 1:
+            argument = parts[1].strip()
+        else:
+            argument = ""
+        
+        if client_addr not in client_sessions:
+            client_sessions[client_addr] = {
+                "streamer": None,
+                "frames": None
+            }
+            
+        session = client_sessions[client_addr]
+        
+        if command == "PLAY":
+            filename = argument.strip()
+            
+            try:
+               session["streamer"] = rtp_streamer.RTPStreamer(client_addr,filename,self.server_socket)
+               session["streamer"].start_stream()
+               self.server_socket.sendto(b"200 OK PLAY", client_addr)
+                
+            except FileNotFoundError:
+                self.server_socket.sendto(b"404 NOT_FOUND", client_addr)
+        
+        elif command == "STOP":
+            if session["streamer"]:
+                session["streamer"].stop_stream()
+            self.server_socket.sendto(b"200 OK STOP", client_addr)
+        else:
+            self.server_socket.sendto(b"400 BAD_REQUEST", client_addr)
